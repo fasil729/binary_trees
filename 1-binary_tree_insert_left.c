337a8496b5af7d8df12085772dff10ff3e9d938a
fasil729 <101599549+fasil729@users.noreply.github.com>
2022-12-16 04:58:01 +0300

    fixing at last

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -17,7 +17,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 	new = calloc(1, sizeof(binary_tree_t));
-
 	if (new == NULL)
 		return (NULL);
 	new->parent = parent;
