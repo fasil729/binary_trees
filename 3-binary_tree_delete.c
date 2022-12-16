2fdd8a84b2199b25b4a20d7b86fa2908d671b499
fasika <fasikafikadu38@gmail.com>
2022-12-15 18:24:25 -0800

    delete

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_delete - delete the whole tree
+ * @tree: s a pointer to the root node of the tree to delete
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree)
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		free(tree);
+	}
+}
