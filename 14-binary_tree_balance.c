3beffa0741bf6dca2c6f973b31dec64a8494a1ea
fasika <fasikafikadu38@gmail.com>
2022-12-16 05:23:08 -0800

    balance

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,39 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: a pointer to the root node of the tree to measure the balance factor
+ * Return: the balance if the tree is NULL 0
+ */
+int height(const binary_tree_t *tree, int h);
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int balance;
+
+	if (tree == NULL)
+		return (0);
+	balance = height(tree->left, 0) - height(tree->right, 0);
+	return (balance);
+}
+
+/**
+ * height - calculate tree height
+ * @tree - a pointer to the root node of the sub-tree 
+ * @h - height of sub-tree
+ * Return: height
+ */
+
+int height(const binary_tree_t *tree, int h)
+{
+	int left, right;
+	if (tree == NULL)
+		return (h);
+	h++;
+	left = height(tree->left, h);
+	right = height(tree->right, h);
+	if (right > left)
+	{
+		return (right);
+	}
+	return (left);
+}
--- main.c
+++ main.c
@@ -10,20 +10,24 @@
 int main(void)
 {
     binary_tree_t *root;
-    size_t leaves;
+    int balance;
 
     root = binary_tree_node(NULL, 98);
     root->left = binary_tree_node(root, 12);
     root->right = binary_tree_node(root, 402);
     binary_tree_insert_right(root->left, 54);
     binary_tree_insert_right(root, 128);
+    binary_tree_insert_left(root, 45);
+    binary_tree_insert_right(root->left, 50);
+    binary_tree_insert_left(root->left->left, 10);
+    binary_tree_insert_left(root->left->left->left, 8);
     binary_tree_print(root);
 
-    leaves = binary_tree_leaves(root);
-    printf("Leaves in %d: %lu\n", root->n, leaves);
-    leaves = binary_tree_leaves(root->right);
-    printf("Leaves in %d: %lu\n", root->right->n, leaves);
-    leaves = binary_tree_leaves(root->left->right);
-    printf("Leaves in %d: %lu\n", root->left->right->n, leaves);
+    balance = binary_tree_balance(root);
+    printf("Balance of %d: %+d\n", root->n, balance);
+    balance = binary_tree_balance(root->right);
+    printf("Balance of %d: %+d\n", root->right->n, balance);
+    balance = binary_tree_balance(root->left->left->right);
+    printf("Balance of %d: %+d\n", root->left->left->right->n, balance);
     return (0);
 }
